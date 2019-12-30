bee5056e0eeaaed7bfbd6c725fd3bc4bef7cb426
Hongtao Zhang <hoz043@ucsd.edu>
2019-12-30 14:50:02 -0800

    add makefile comments

---
+++ Build/event_groups.d
@@ -0,0 +1,41 @@
+Build/event_groups.o: \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ Firmware/Core/Inc/FreeRTOSConfig.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
+ Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+Firmware/Core/Inc/FreeRTOSConfig.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
+
+Firmware/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
